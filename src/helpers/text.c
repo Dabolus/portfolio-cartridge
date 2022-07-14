af03653801a6bd13316568e812b330aa63bf6ce5
Giorgio Garasto <giorgio@garasto.me>
2022-07-14 22:07:40 +0200

    chore: dynamically compute max text length based on screen width

--- src/helpers/text.c
+++ src/helpers/text.c
@@ -6,6 +6,6 @@
 
 #define ARRAY_LEN(A) sizeof(A) / sizeof(A[0])
 #define PRINT_TEXT(X, Y, T) set_bkg_tiles(X, Y, ARRAY_LEN(T), 1, T)
-#define TYPEWRITTEN_TEXT_MAX_LENGTH 18
+#define TYPEWRITTEN_TEXT_MAX_LENGTH (DEVICE_SCREEN_WIDTH - 2)
 
 #endif
