87d8bc066ce5e9f2a293a74fdbe5c3f9cd171591
Giorgio Garasto <giorgio@garasto.me>
2022-07-20 23:32:47 +0200

    feat: implement home menu background

--- src/pages/home_menu.c
+++ src/pages/home_menu.c
@@ -1,6 +1,7 @@
 #ifndef _PAGES_HOME_MENU_
 #define _PAGES_HOME_MENU_
 
+#include "../assets/home_menu.c"
 #include <gbdk/platform.h>
 
 void home_menu_setup() {
@@ -9,6 +10,11 @@ void home_menu_setup() {
       set_bkg_tile_xy(i, j, 0x00);
     }
   }
+  set_bkg_data(50, 8, home_menu_frame_data);
+  set_bkg_tiles(0, 0, 10, 9, home_menu_frame_map);
+  set_bkg_tiles(10, 0, 10, 9, home_menu_frame_map);
+  set_bkg_tiles(0, 9, 10, 9, home_menu_frame_map);
+  set_bkg_tiles(10, 9, 10, 9, home_menu_frame_map);
 }
 
 void home_menu_loop(uint8_t *current_loop_count) { current_loop_count = 0; }
