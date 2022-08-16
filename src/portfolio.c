55eb03b1a2b54c88cd7bbffa75c2679588eb8a95
Giorgio Garasto <giorgio@garasto.me>
2022-08-16 23:01:26 +0200

    feat: setup gbc palettes

--- src/portfolio.c
+++ src/portfolio.c
@@ -11,12 +11,32 @@
 #include "pages/skills.h"
 #include <gbdk/platform.h>
 
+static const palette_color_t light_palette[] = {
+    RGB8(250, 250, 250),
+    RGB8(240, 240, 240),
+    RGB8(220, 220, 220),
+    RGB8(32, 32, 32),
+};
+
+static const palette_color_t dark_palette[] = {
+    RGB8(33, 33, 33),
+    RGB8(48, 48, 48),
+    RGB8(73, 73, 73),
+    RGB8(255, 255, 255),
+};
+
+BOOLEAN is_dark_theme = FALSE;
+
 void handle_theme_change() {
-  // Reverse the palette bits
+  // Reverse the palette bits (for Game Boy Classic)
   BGP_REG = ~BGP_REG;
+  // Switch between the two color palettes (for Game Boy Color)
+  is_dark_theme = !is_dark_theme;
+  set_bkg_palette(0, 1, is_dark_theme ? dark_palette : light_palette);
 }
 
 void main() {
+  set_bkg_palette(0, 1, light_palette);
   // Load font in the background data (2 colors -> 1 bit per pixel)
   set_bkg_1bpp_data(0, 50, font_early_game_boy);
   init_page(HOME_HEADER);
