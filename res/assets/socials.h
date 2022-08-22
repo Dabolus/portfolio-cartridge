bf172f996ea3ebc7373cb8c78d3aaeb145ed6f45
Giorgio Garasto <giorgio@garasto.me>
2022-08-22 19:51:11 +0200

    feat: add colors to socials icons

--- res/assets/socials.h
+++ res/assets/socials.h
@@ -2,28 +2,37 @@
 #ifndef ASSETS_SOCIALS_H
 #define ASSETS_SOCIALS_H
 
+#include <gbdk/platform.h>
+
 extern const unsigned char email_qr_data[392];
 extern const unsigned char email_qr_tiles[49];
 
 extern const unsigned char google_developers_data[64];
 extern const unsigned char google_developers_tiles[4];
+extern const palette_color_t google_developers_color;
 
 extern const unsigned char github_data[64];
 extern const unsigned char github_tiles[4];
+extern const palette_color_t github_color;
 
 extern const unsigned char twitter_data[64];
 extern const unsigned char twitter_tiles[4];
+extern const palette_color_t twitter_color;
 
 extern const unsigned char telegram_data[64];
 extern const unsigned char telegram_tiles[4];
+extern const palette_color_t telegram_color;
 
 extern const unsigned char polywork_data[64];
 extern const unsigned char polywork_tiles[4];
+extern const palette_color_t polywork_color;
 
 extern const unsigned char linkedin_data[64];
 extern const unsigned char linkedin_tiles[4];
+extern const palette_color_t linkedin_color;
 
 extern const unsigned char facebook_data[64];
 extern const unsigned char facebook_tiles[4];
+extern const palette_color_t facebook_color;
 
 #endif
--- src/pages/contacts.c
+++ src/pages/contacts.c
@@ -21,31 +21,66 @@ void contacts_setup() {
   // Socials
   // Google Developers
   set_sprite_data(0, 4, google_developers_data);
+  for (uint8_t i = 0; i < 4; ++i) {
+    set_sprite_prop(i, 1);
+  }
+  set_sprite_palette_entry(1, 2, google_developers_color);
+  set_sprite_palette_entry(1, 3, RGB8(0, 0, 0));
   set_sprite_tiles(4, 28, 2, 2, google_developers_tiles);
   set_bkg_tiles(3, 4, ARRAY_LEN(gga), 1, gga);
   // GitHub
   set_sprite_data(4, 4, github_data);
+  for (uint8_t i = 0; i < 4; ++i) {
+    set_sprite_prop(i + 4, 1);
+  }
+  set_sprite_palette_entry(1, 1, github_color);
+  set_sprite_palette_entry(1, 3, RGB8(0, 0, 0));
   set_sprite_tiles(4, 44, 2, 2, github_tiles);
   set_bkg_tiles(3, 6, ARRAY_LEN(dabolus), 1, dabolus);
   // Twitter
   set_sprite_data(8, 4, twitter_data);
+  for (uint8_t i = 0; i < 4; ++i) {
+    set_sprite_prop(i + 8, 2);
+  }
+  set_sprite_palette_entry(2, 2, twitter_color);
+  set_sprite_palette_entry(2, 3, RGB8(0, 0, 0));
   set_sprite_tiles(4, 60, 2, 2, twitter_tiles);
   set_bkg_tiles(3, 8, ARRAY_LEN(dabolus), 1, dabolus);
   // Telegram
   set_sprite_data(12, 4, telegram_data);
+  for (uint8_t i = 0; i < 4; ++i) {
+    set_sprite_prop(i + 12, 2);
+  }
+  set_sprite_palette_entry(2, 1, telegram_color);
+  set_sprite_palette_entry(2, 3, RGB8(0, 0, 0));
   set_sprite_tiles(4, 76, 2, 2, telegram_tiles);
   set_bkg_tiles(3, 10, ARRAY_LEN(dabolus), 1, dabolus);
   // Polywork
   set_sprite_data(16, 4, polywork_data);
+  for (uint8_t i = 0; i < 4; ++i) {
+    set_sprite_prop(i + 16, 3);
+  }
+  set_sprite_palette_entry(3, 2, polywork_color);
+  set_sprite_palette_entry(3, 3, RGB8(0, 0, 0));
   set_sprite_tiles(4, 92, 2, 2, polywork_tiles);
   set_bkg_tiles(3, 12, ARRAY_LEN(dabolus), 1, dabolus);
   // LinkedIn
   set_sprite_data(20, 4, linkedin_data);
+  for (uint8_t i = 0; i < 4; ++i) {
+    set_sprite_prop(i + 20, 3);
+  }
+  set_sprite_palette_entry(3, 1, linkedin_color);
+  set_sprite_palette_entry(3, 3, RGB8(0, 0, 0));
   set_sprite_tiles(4, 108, 2, 2, linkedin_tiles);
   set_bkg_tiles(3, 14, ARRAY_LEN(giorgio), 1, giorgio);
   set_bkg_tiles(3 + ARRAY_LEN(giorgio), 14, ARRAY_LEN(garasto), 1, garasto);
   // Facebook
   set_sprite_data(24, 4, facebook_data);
+  for (uint8_t i = 0; i < 4; ++i) {
+    set_sprite_prop(i + 24, 4);
+  }
+  set_sprite_palette_entry(4, 2, facebook_color);
+  set_sprite_palette_entry(4, 3, RGB8(0, 0, 0));
   set_sprite_tiles(4, 124, 2, 2, facebook_tiles);
   set_bkg_tiles(3, 16, ARRAY_LEN(giorgio), 1, giorgio);
   set_bkg_tile_xy(3 + ARRAY_LEN(giorgio), 16, 0x08);
