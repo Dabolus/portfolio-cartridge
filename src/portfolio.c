72b4cee9cce13ce7811b176a49d81cd0646dd79f
Giorgio Garasto <giorgio@garasto.me>
2022-08-24 18:32:15 +0200

    refactor: handle theme using new theming helper

--- src/portfolio.c
+++ src/portfolio.c
@@ -1,6 +1,7 @@
 #include "../res/fonts/font_early_game_boy.h"
 #include "helpers/fade.h"
 #include "helpers/text.h"
+#include "helpers/theme.h"
 #include "helpers/utils.h"
 #include "pages/about.h"
 #include "pages/contacts.h"
@@ -11,35 +12,8 @@
 #include "pages/skills.h"
 #include <gbdk/platform.h>
 
-BOOLEAN is_dark_theme = FALSE;
-
-void handle_theme_change() {
-  if (DEVICE_SUPPORTS_COLOR) {
-    // Replace the colors in the default palette with the ones from the theme in
-    // the proper order
-    is_dark_theme = !is_dark_theme;
-    for (uint8_t i = 0; i < ARRAY_LEN(rgb_theme_colors); ++i) {
-      const uint8_t color_index =
-          is_dark_theme ? ARRAY_LEN(rgb_theme_colors) - i - 1 : i;
-      set_bkg_palette_entry(0, i, rgb_theme_colors[color_index]);
-      set_sprite_palette_entry(0, i, rgb_theme_colors[color_index]);
-    }
-  } else {
-#ifdef NINTENDO
-    // Reverse the background and sprites palettes bits (for Game Boy Classic)
-    BGP_REG = ~BGP_REG;
-    OBP0_REG = ~OBP0_REG;
-#endif
-  }
-}
-
 void main() {
-  if (DEVICE_SUPPORTS_COLOR) {
-    for (uint8_t i = 0; i < ARRAY_LEN(rgb_theme_colors); ++i) {
-      set_bkg_palette_entry(0, i, rgb_theme_colors[i]);
-      set_sprite_palette_entry(0, i, rgb_theme_colors[i]);
-    }
-  }
+  init_theme();
   // Load font in the background data (2 colors -> 1 bit per pixel)
   set_bkg_1bpp_data(0, 50, font_early_game_boy);
   init_page(HOME_HEADER);
@@ -51,7 +25,7 @@ void main() {
     uint8_t keys = joypad();
 
     // Allow switching theme by pressing Select
-    throttlekey(keys, J_SELECT, &handle_theme_change);
+    throttlekey(keys, J_SELECT, &switch_theme);
 
     // Allow going back to the home header from any page
     // by pressing Start
